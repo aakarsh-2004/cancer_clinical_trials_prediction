f553c16383259d3a9edc941324913c2f54449eab
Aakarsh Beohar <aakarshbeohar2004@gmail.com>
2023-12-24 19:28:35 +0530

    Update connectDB.js

--- node_backend/connectDB.js
+++ node_backend/connectDB.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 // Connecting the MongoDB database
 function connectDB() {
     mongoose
-    .connect("mongodb+srv://mannuhere06:0624haishayad@admin-abhi.zbvwevw.mongodb.net/?retryWrites=true&w=majority")
+    .connect()
     .then((e) => console.log(`Connected to mongoDB: ${e.connection.host}`))
     .catch((e) => console.log(e));
 }
